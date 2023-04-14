da5c2012efd5679a2a4d9d725bd21043590752a2
Francisco Viana <franciscosantos29@gmail.com>
2023-04-14 20:37:53 -0300

    Correção no VendaQueryServiceImpl na resposta

--- src/main/java/com/fsv/algafood/infrastructure/repository/service/query/VendaQueryServiceImpl.java
+++ src/main/java/com/fsv/algafood/infrastructure/repository/service/query/VendaQueryServiceImpl.java
@@ -46,11 +46,11 @@ public class VendaQueryServiceImpl implements VendaQueryService {
 		}
 		
 		if (filtro.getDataCriacaoInicio() != null) {
-			predicates.add(builder.equal(root.get("dataCriacao"), filtro.getDataCriacaoInicio()));
+			predicates.add(builder.greaterThanOrEqualTo(root.get("dataCriacao"), filtro.getDataCriacaoInicio()));
 		}
 		
 		if (filtro.getDataCriacaoFim() != null) {
-			predicates.add(builder.equal(root.get("dataCriacao"), filtro.getDataCriacaoFim()));
+			predicates.add(builder.lessThanOrEqualTo(root.get("dataCriacao"), filtro.getDataCriacaoFim()));
 		}
 		
 		predicates.add(root.get("status")
